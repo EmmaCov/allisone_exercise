3386998ac0118fbdb90896f2ca30207697d7fab1
EmmaCov <45871498+EmmaCov@users.noreply.github.com>
2022-02-16 16:12:33 +0100

    Add of some unit tests and notebook
    The notebook was used in first intention to analyse the dataset (visualisation and preprocessing steps)

---
+++ test.py
@@ -0,0 +1,25 @@
+import unittest
+import numpy as np
+
+import main
+from preprocess import preprocess
+
+model = main.DiamondModel()
+
+class APITestCase(unittest.TestCase):
+
+    def test_score(self):
+        result = model.score()
+        self.assertTrue(result>=0.97)
+
+    def test_preprocess(self):
+        data = [1.01, "Ideal", "G", "VS2", "VG", "EX", "GIA"]
+        processed_data = preprocess(data)
+        expected_output = np.array([1.01, 2.00, 3.00, 4.00, 3.00, 0.00, 1.00])
+        
+        self.assertEqual(len(processed_data), len(expected_output))
+        for i in range(len(processed_data)):
+            self.assertEqual(processed_data[i], expected_output[i])
+
+if __name__ == '__main__':
+    unittest.main()
